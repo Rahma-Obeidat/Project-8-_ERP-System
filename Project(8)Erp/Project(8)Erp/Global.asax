583a56e89ff46c761eec907267e3519dd0f15dbe
unknown <you@example.com>
2023-02-15 09:24:49 +0300

    p

---
+++ Project(8)Erp/Project(8)Erp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_8_Erp.MvcApplication" Language="C#" %>
---
+++ Project(8)Erp/Project(8)Erp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_8_Erp.Startup))]
+namespace Project_8_Erp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
